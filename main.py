fe2b186523bbe5b9a5b20482410e7a24579b7198
vld <nvladislav@x2sy.com>
2016-05-13 17:36:11 +0300

    Basic templates

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import configparser
 
 
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 # Транспортні засоби у розшуку. ID - Ідентифікатор. OVD - Регіон (орган внутрішніх справ).
@@ -21,11 +21,18 @@ def search_nom(nom):
     number_en = nom.translate(ru_to_en)
     return autos.find({'NOM': {'$regex': '(%s|%s)' % (number_ru, number_en)}})
 
+@app.route('/search', methods=['POST'])
+def search():
+    print(request.form)
+    results = search_nom(request.form['text'])
+    return render_template('results.html', results=results)
+
 
 @app.route("/")
 def hello():
     # Law Enforcement
-    return "Hello World!\nAll rights for the photo reserved by Thomas Leuthard(https://www.flickr.com/photos/thomasleuthard/)"
+    # return "Hello World!\nAll rights for the photo reserved by Thomas Leuthard(https://www.flickr.com/photos/thomasleuthard/)"
+    return render_template('main.html')
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
@@ -34,4 +41,4 @@ if __name__ == "__main__":
     mongo_client = pymongo.MongoClient(host=config['mongodb']['host'], port=int(config['mongodb']['port']))
     autos = mongo_client.mvs_wanted.autos
 
-    app.run()
+    app.run(debug=True)
