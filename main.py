c3c994f40d81e5bb1e9237c305f8ae3a3b2c3bb0
vld <nvladislav@x2sy.com>
2016-05-13 17:04:24 +0300

    Add 2 translate transforms

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ app = Flask(__name__)
 # Транспортні засоби у розшуку. ID - Ідентифікатор. OVD - Регіон (орган внутрішніх справ).
 # NSH - Номер шасі. NOM - Державний номер. NKU - Номер кузова. MDL - Марка,модель. COLOR - Колір.
 
+# (LATIN,CYRILLIC)
 symbols = (u"ABCEHIKMOPTX", u"АВСЕНІКМОРТХ")
-tr = {ord(a):ord(b) for a, b in zip(*symbols)}
+en_to_ru = {ord(a):ord(b) for a, b in zip(*symbols)}
+ru_to_en = {ord(b):ord(a) for a, b in zip(*symbols)}
 
 
 def search_nom(nom):
-    number = nom.translate(tr)
-    autos.find({'NOM': {'$regex': number}})
+    number_ru = nom.translate(en_to_ru)
+    number_en = nom.translate(ru_to_en)
+    return autos.find({'NOM': {'$regex': '(%s|%s)' % (number_ru, number_en)}})
 
 
 @app.route("/")
 def hello():
+    # Law Enforcement
     return "Hello World!\nAll rights for the photo reserved by Thomas Leuthard(https://www.flickr.com/photos/thomasleuthard/)"
 
 if __name__ == "__main__":
