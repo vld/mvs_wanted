342ed82b84b6b88e7e96f43b2866d644b795710e
vld <nvladislav@x2sy.com>
2018-02-10 19:27:18 +0200

    Add revision to filename

--- load_data.py
+++ load_data.py
@@ -8,15 +8,19 @@ DATASET_ID = '589bb2f6-682c-4679-a81c-4070d92d8eef'
 def load_data():
     description = urllib.request.urlopen('http://data.gov.ua/view-dataset/dataset.json?dataset-id=%s' % DATASET_ID).read()
     dc = json.loads(description.decode('utf-8'))
+    revisions = dc.get('revisions', [{}])
+    last_rev = revisions[0].get('revision_id', '0')
     json_files = filter(lambda file: file['format'] == 'json', dc['files'])
     url = [f['url'] for f in json_files][0]
-    filename = url.split('/')[-1]
-    filepath = 'data/%s' % (filename)
+    file_name, file_ext = os.path.splitext(url.split('/')[-1])
+    filepath = 'data/%s_%s%s' % (file_name, last_rev, file_ext)
     if (not os.path.exists(filepath)) | (not os.path.isfile(filepath)):
         os.system('curl -o %s %s' % (filepath, url))
         if 'autos' in mongo_client.mvs_wanted.collection_names():
+            print('Drop old db')
             mongo_client.mvs_wanted.autos.drop()
-        os.system('mongoimport --db mvs_wanted --collection autos --file %s' % filepath)
+        os.system('mongoimport --db mvs_wanted --collection autos --jsonArray %s' % filepath)
+        print('Load new data')
         mongo_client.mvs_wanted.autos.create_index([("NKU", pymongo.ASCENDING)], background=True)
         mongo_client.mvs_wanted.autos.create_index([("NSH", pymongo.ASCENDING)], background=True)
         mongo_client.mvs_wanted.autos.create_index([("NOM", pymongo.ASCENDING)], background=True)
